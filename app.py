b4e3299a4f74cbbb0ef1d9ed0b30072811b24221
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-05 18:27:23 +0200

    Add homepage route to render the main chat interface

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
