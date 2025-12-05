ac199d5c0a96e55086c2d706e719829f8e977edf
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-05 18:01:30 +0200

    Simplify basic server setup

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase, digits
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
