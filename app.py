0ac52cc59bd81ee0179eef40cda86ed1bfd7e30f
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-06 00:14:52 +0200

    Grab form data and display error when inputs are empty

--- app.py
+++ app.py
@@ -12,6 +12,22 @@ socketio = SocketIO(app)
 
 @app.route("/", methods=["POST", "GET"])
 def home():
+    # Grab our form data
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        # generate = request.form.get("generate", False) # return False if generate doem't exist
+        # connect = request.form.get("connect", False) # return False if connect doesn't exist
+
+        # Check if the user did not pass a name
+        if not name:
+            return render_template("home.html", error="Username cannot be empty.")
+        
+        # Check if the user did not pass a room code
+        if not code:
+            return render_template("home.html", error="The room code cannot be empty.")
+
+
     return render_template("home.html")
 
 if __name__ == "__main__":
