1007d164ef8ad7a73270c3416d35fd6dc32b49e2
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-06 19:25:09 +0200

    Implement socket connection handler for room joining

--- app.py
+++ app.py
@@ -72,5 +72,23 @@ def room():
     
     return render_template("room.html", code_badge=room)
 
+@socketio.on("connect")
+def connect(auth):
+    # Look for user's room and name
+    room = session.get("room")
+    name = session.get("name")
+
+    if not room or not name:
+        return
+    
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"name": name, "message": "has entered the room"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"{name} joined room {room}")
+
 if __name__ == "__main__":
     socketio.run(app, debug=True)
