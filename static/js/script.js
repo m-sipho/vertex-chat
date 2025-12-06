09408aa575e0f68d5e332822d5f19410fed58621
m-sipho <241426803+m-sipho@users.noreply.github.com>
2025-12-06 21:56:06 +0200

    Add createNotification function and socket message listener

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,22 @@
-const socket = io()
+var socketio = io()
+
+const messages = document.querySelector(".messages-area");
+
+function createNotification(name, msg) {
+    const content = `
+        <div class="join-notification">
+            <span class="notify-text">
+                <span class="name">${name}</span> ${msg}
+            </span>
+        </div>
+    `
+    messages.insertAdjacentHTML('beforeend', content);
+    messages.scrollTop = messages.scrollHeight;
+}
+
+socketio.on("message", (data) => {
+    createNotification(data.name, data.message);
+});
 
 function sendMessage() {
     console.log("send");
